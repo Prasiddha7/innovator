85a6db26d00b4e0dce3153be81cb7fdbed2b66a5
Prasiddha237 <prasiddha.sbd@trilokyatech.com>
2026-03-15 11:19:55 +0545

    Fix missing TEMPLATES configuration for social_media_service

--- social_media_service/social_media_service/settings/base.py
+++ social_media_service/social_media_service/settings/base.py
@@ -54,6 +54,22 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'social_media_service.urls'
 WSGI_APPLICATION = 'social_media_service.wsgi.application'
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
